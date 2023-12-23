a74d0f4db55bdad68347d96e1a6c04ba6933b6be
UmairFarooqLone <umairibnfarooq1@gmail.com>
2023-12-24 00:09:42 +0530

    Additions To Blog

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,9 +1,14 @@
 const Blog = require('../models/blog');
 
-exports.blog_list =  async function(req, res) {
-    const blogs= await Blog.find()
-  
+exports.blog_list = async function(req, res) {
+    let blogs = await Blog.find({}, 'title author');
     res.render('blogList', { blogs: blogs });
+};
+
+// Add a new function to get a specific blog by id
+exports.blog_detail = async function(req, res) {
+    let blog = await Blog.findById(req.params.id);
+    res.render('blogDetail', { blog: blog });
 };
 
 exports.blog_create_get = function(req, res) {
@@ -15,5 +20,9 @@ exports.blog_create_get = function(req, res) {
 exports.blog_create_post = async function(req, res) {
     const blog = new Blog(req.body);
     await blog.save()
+    res.redirect('/dashboard/blog');
+};
+exports.blog_delete = async function(req, res) {
+    await Blog.findByIdAndDelete(req.params.id);
     res.redirect('/dashboard/blog');
 };
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -5,5 +5,6 @@ const blog_controller = require('../controllers/blogController');
 router.get('', blog_controller.blog_list);
 router.get('/create', blog_controller.blog_create_get);
 router.post('/create', blog_controller.blog_create_post);
-
+router.get('/:id', blog_controller.blog_detail);
+router.get('/delete/:id', blog_controller.blog_delete);
 module.exports = router;
