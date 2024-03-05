2acc4ba25f749a2c9b45983ea4ab8268db9a01dc
UmairFarooqLone <umairibnfarooq1@gmail.com>
2024-03-05 15:44:02 +0530

    Fix Bug when clicking Read more on a blog

--- controllers/blogPageController.js
+++ controllers/blogPageController.js
@@ -8,5 +8,6 @@ exports.blog_index = async function(req, res) {
 
 exports.blog_detail = async function(req, res) {
     let blog = await Blog.findById(req.params.id);
-    res.render('blogPageDetail', { blog: blog });
+    const loggedin=req.session.user;
+    res.render('blogPageDetail', { blog: blog,loggedin });
 };
